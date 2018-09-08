d3f047aee213ba10b888067a8284df63b41a93e3
e.b.kitaev <evkitaev@gmail.com>
2018-09-08 12:00:17 +0300

    Get test

---
+++ src/test/java/SecondPackage/BaseTest.java
@@ -0,0 +1,30 @@
+package SecondPackage;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.given;
+
+public class BaseTest {
+    public static String token;
+    @BeforeAll
+    public static void authorization(){
+        token = given()
+                .baseUri("https://tcrm-api-test.tcsbank.ru/")
+                .basePath("uaa")
+                .formParam("grant_type", "password")
+                .formParam("username", "tcrm_sme_auto")
+                .formParam("password", "PnR6RN$7s26D6tCh")
+                .headers("Authorization", "Basic d2ViX2FwcDo=")
+                .contentType("application/x-www-form-urlencoded")
+                .post("/oauth/token")
+                .then()
+                .log().all()
+                .assertThat()
+                .extract().jsonPath().getString("access_token");
+        System.out.println(token);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(token);
+    }
+}
---
+++ src/test/java/SecondPackage/RequestTest.java
@@ -0,0 +1,21 @@
+package SecondPackage;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class RequestTest extends BaseTest {
+    @Test
+    public void GetRequestTest(){
+        given()
+        .log().all()
+                .baseUri("https://tcrm-api-test.tcsbank.ru")
+                .basePath("sme/api/v1")
+                .header("Authorization", "Bearer " + token)
+                .get("/account/5-5FRVACNQ")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .log().all();
+    }
+}
